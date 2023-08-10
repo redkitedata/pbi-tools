d826f3038dedece5a0bcd4de751b7632f993c279
TheAndyShao <89639042+TheAndyShao@users.noreply.github.com>
2023-08-10 16:50:00 +0100

    Removing helper function

--- src/pbi/tools.py
+++ src/pbi/tools.py
@@ -1,6 +1,8 @@
 import os
-from git import Repo
 import zipfile as zf
+
+from git import Repo
+
 
 def handle_request(r, allowed_codes=None):
     if not allowed_codes:
@@ -83,18 +85,3 @@ def open_branches():
             os.path.abspath(__file__), search_parent_directories=True
         ).remotes.origin.refs
     ]
-
-def build_refresh_object(tables):
-    """Builds the objects array used by the enhanced refresh API
-
-    :param tables: a list of tables to be refreshed
-    :return: an array of tables
-    """
-
-    array = {"objects": []}
-
-    for table in tables:
-        entry = {"table":table}
-        array["objects"].append(entry)
-
-    return array
